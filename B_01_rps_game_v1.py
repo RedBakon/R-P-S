6dd9b0dbde8908b27d833afcbffbc7e362d03e2a
RedBakon <pamae3421@masseyhigh.school.nz>
2025-03-03 14:07:12 +1300

    Add files via upload

--- B_01_rps_game_v1.py
+++ B_01_rps_game_v1.py
@@ -1,4 +1,56 @@
-# checks for an integer more than 0 (allows <enter>)
+import random
+
+
+# Check that users have entered a valid
+# option based of a list
+def string_checker(question, valid_ans=('yes', 'no')):
+    error = f"Please enter a valid option from the following list: {valid_ans}"
+
+    while True:
+
+        # Get user response and make sure it's lowercase
+        user_response = input(question).lower()
+
+        for item in valid_ans:
+            # check if the user response is a word in the list
+            if item == user_response:
+                return item
+
+            # check if the user response is the same as
+            # the first letter of an item in the list
+            elif user_response == item[0]:
+                return item
+
+        # print error if user does not enter something that is valid
+        print(error)
+        print()
+
+
+# Displays instructions
+def instructions():
+    """Prints instructions"""
+
+    print("""
+*** Instructions ****
+
+To begin, choose the number of rounds (press <enter> for
+infinite mode).
+
+The play against the computer. You need to choose R (rock
+P (paper) or S (scissors).
+
+The rules are as follows:
+o   Paper beats rock
+o   Rock beats scissors
+o   Scissors beats paper
+
+Press <xxx> to end the game at anytime.
+
+Good Luck!
+    """)
+
+
+# checks for an integer m46ore than 0 (allows <enter>)
 def int_check(question):
     while True:
         error = "Please enter an integer that is 1 or more"
@@ -22,41 +74,86 @@ def int_check(question):
             print(error)
 
 
+# compares user / computer choice and returns
+# result (win / lose / tie)
+def rps_compare(user, comp):
+    # If the user and the computer choice is the same, it's a tie
+    if user == comp:
+        result = "tie"
+
+    # There are three ways to win
+    elif user == "paper" and comp == "rock":
+        result = "win"
+    elif user == "scissors" and comp == "paper":
+        result = "win"
+    elif user == "rock" and comp == "scissors":
+        result = "win"
+
+    # if it's not a win / tie, then it's a loss
+    else:
+        result = "lose"
+
+    return result
+
+
 # Main Routine Starts here
 
 # Initialize game variables
 mode = "regular"
 rounds_played = 0
 
+rps_list = ["rock", "paper", "scissors", "xxx"]
 
 print("🪨📄✂️ Rock / Paper / Scissors Game 🪨📄✂️")
 print()
 
-# Instructions
+# ask the user if they want instructions (check they say yes / no)
+# them if requested
+want_instructions = string_checker("Do you want to see the instructions? ")
+
+# Display the instructions if the usr wants to see them...
+if want_instructions == "yes":
+    instructions()
 
 # Ask user for number of rounds / infinite mode
 num_rounds = int_check("How many rounds would you like? Push <enter> for infinite mode: ")
 
 if num_rounds == "infinite":
     mode = "infinite"
-    print("you chose infinite")
     num_rounds = 5
 
 # Game loop starts here
 while rounds_played < num_rounds:
-    user_choice = input("Choose: ")
 
+    # Rounds headings
+    if mode == "infinite":
+        rounds_heading = f"\n♾️♾️♾️ Round {rounds_played + 1} (Infinite Mode) ♾️♾️♾️"
+    else:
+        rounds_heading = f"\n💿💿💿 Round {rounds_played + 1} of {num_rounds} 💿💿💿"
+
+    print(rounds_heading)
+
+    # randomly choose from the rps list (excluding the exit code)
+    comp_choice = random.choice(rps_list[:-1])
+    print("Computer choice", comp_choice)
+
+    # get user choice
+    user_choice = string_checker("Choose: ", rps_list)
+    print("you chose", user_choice)
+
+    # If user choice is the exit code, break the loop
     if user_choice == "xxx":
         break
-        
+
+    result = rps_compare(user_choice, comp_choice)
+    print(f"{user_choice} vs {comp_choice}, {result}")
+
     rounds_played += 1
-    print("rounds played: ", rounds_played)
 
     # if users are in infinite mode, increase number of rounds!
     if mode == "infinite":
         num_rounds += 1
 
-    print("num rounds: ", num_rounds)
 # Game loop end here
 
 # Game History / Statistics area
